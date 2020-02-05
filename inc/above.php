f2d2980b86dad80b991c980876f02eab9f1a9973
adi <adi@lazaro.in>
2020-02-05 18:02:25 +0530

    Header files : Move all the work from head.php to above.php and then some more

--- inc/above.php
+++ inc/above.php
@@ -11,14 +11,56 @@ require_once __DIR__ . '/lazaro.php';
 /*
  * A version number for versioning assets to invalidate the browser cache
  */
-$ver = '?v=20181126';
+$ver = '?v=20200103';
 
-// Pull some data from the request
-$urlSlug = $_GET[ '_slug' ] ?? null;
-$postType = $_GET[ '_post_type' ] ?? null;
+/*
+ * Get all the links on the site
+ */
+$defaultLinks = require __DIR__ . '/default-nav-links.php';
+$links = getContent( $defaultLinks, 'pages' );
 
+/*
+ * Figure out the base URL
+ * 	We diff the document root and the directory of this file to determine it
+ */
+$pathFragments = array_values( array_filter( explode( '/', substr( __DIR__, strlen( $_SERVER[ 'DOCUMENT_ROOT' ] ) ) ) ) );
+if ( count( $pathFragments ) > 1 )
+	$baseURL = '/' . $pathFragments[ 0 ] . '/';
+else
+	$baseURL = '/';
+
+/*
+ * Get the title and URL of the website and current page
+ */
+if ( cmsIsEnabled() ) {
+	$thePost = getCurrentPost( $urlSlug, $postType );
+	if ( empty( $thePost ) and $postType !== 'page' ) {
+		// echo 'Please create a corresponding page or post with the slug' . '"' . $urlSlug . '"' . 'in the CMS.';
+		http_response_code( 404 );
+		return header( 'Location: /', true, 302 );
+		exit;
+	}
+	else if ( ! empty( $thePost ) )
+		$postId = $thePost->ID;
+}
+
+
+// Construct the page's title ( for use in the title tag )
+$siteTitle = getContent( '', 'page_title', $urlSlug ) ?: getContent( 'Guesture', 'page_title' );
+$pageUrl = $siteUrl . $requestPath;
+
+if ( cmsIsEnabled() and ! empty( $thePost ) )
+	$pageTitle = $thePost->post_title . ' | ' . $siteTitle;
+else
+	$pageTitle = $siteTitle;
+
+
+// Get the page's image for SEO and other related purposes
 $pageImage = getContent( '', 'page_image', $urlSlug ) ?: getContent( '', 'page_image' );
-$pageImage = $pageImage[ 'sizes' ][ 'medium' ] ?: $pageImage[ 'sizes' ][ 'thumbnail' ] ?: $pageImage[ 'url' ];
+if ( ! empty( $pageImage[ 'sizes' ] ) )
+	$pageImage = $pageImage[ 'sizes' ][ 'medium' ] ?: $pageImage[ 'sizes' ][ 'thumbnail' ] ?: $pageImage[ 'url' ];
+else
+	$pageImage = $pageImage[ 'url' ] ?? null;
 
 // #fornow
 // Just so that when some social media service (WhatsApp) try to ping URL,
@@ -46,7 +88,7 @@ http_response_code( 200 );
 
 	<div id="page-wrapper"><!-- Page Wrapper -->
 
-		<?php require_once 'navigation.php'; ?>
+		<?php // require_once 'navigation.php'; ?>
 
 		<!-- Page Content -->
 		<div id="page-content">
